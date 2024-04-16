1dc591dbae0f1c8a3677d4969c8b5e7e00665bc5
Krys Frost <librarykrystal@gmail.com>
2024-04-16 16:05:34 -0500

    Individually coded projects have been replaced by mapping over an array of project data.

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -123,183 +123,58 @@ function Projects() {
 
       <div className="projectsOuterContainer">
         <div className="projectsInnerContainer">
-
-        {/* THE RUNDOWN */}
-        <div className="projectContainer">
-            <div className="projectHeaderBox">
-              <Typography className="projectHeaderText" variant="h5" mt={0} mb={0} fontWeight="bold" gutterBottom>
-                Fine Art Portfolio Site
-              </Typography>
-              <Typography className="projectHeaderText" variant="body1" mt={0} mb={0} gutterBottom>
-                Deployed website showcasing artwork
-              </Typography>
-            </div>
-            <div className="projectBody">
-              <div className="projectImages">
-                <img className="appImage1" src={artSiteLanding}></img>
-                {/* <img className="appImageRundown" src={rundownDetails}></img> */}
-                {/* <img className="appImageRundown" src={rundownEdit}></img> */}
+          {projects.map(project => {
+            return(
+              <div key={project.id} className="projectContainer">
+                {/* HEADING */}
+                <div className="projectHeaderBox">
+                  <Typography className="projectHeaderText" variant="h5" mt={0} mb={0} fontWeight="bold" gutterBottom>
+                    {project.heading}
+                  </Typography>
+                  {/* SUBHEADING */}
+                  <Typography className="projectHeaderText" variant="body1" mt={0} mb={0} gutterBottom>
+                    {project.subheading}
+                  </Typography>
+                </div>
+                <div className="projectBody">
+                  {/* IMAGE(S) */}
+                  <div className="projectImages">
+                    <img className="appImage1" src={project.image}></img>
+                  </div>
+                  {/* DESCRIPTION */}
+                  <div className="projDescText">
+                    <Typography variant="body1" mt={0} mb={1} gutterBottom>
+                    {project.desc}
+                    </Typography>
+                  </div>
+                  <div className="projectButtons">
+                    {/* WEB LINK BUTTON — conditional, renders only if there is a web link */}
+                    {project.webLink &&
+                      <Button
+                      sx={{ ml: 1, mr: 1, pr:1.5 }}
+                        variant="contained"
+                        color="info"
+                        size="small"
+                        startIcon={<BrushIcon />}
+                        href={project.webLink}
+                        >Visit the website
+                      </Button>
+                    }
+                    {/* GITHUB LINK BUTTON */}
+                    <Button
+                    sx={{ ml: 1, mr: 1 }}
+                      variant="contained"
+                      color="info"
+                      size="small"
+                      startIcon={<GitHubIcon />}
+                      href={project.gitLink}
+                      >See it on GitHub
+                    </Button>
+                  </div>
+                </div>
               </div>
-              <div className="projDescText">
-                <Typography variant="body1" mt={0} mb={1} gutterBottom>
-                This personal portfolio site, built with React, features paintings, photography, and updates on my latest and in-progress work.
-                </Typography>
-              </div>
-              <div className="projectButtons">
-                <Button
-                sx={{ ml: 1, mr: 1, pr:1.5 }}
-                  variant="contained"
-                  color="info"
-                  size="small"
-                  startIcon={<BrushIcon />}
-                  href="https://www.galleryoffrost.com"
-                  >Visit the website
-                </Button>
-                <Button
-                sx={{ ml: 1, mr: 1 }}
-                  variant="contained"
-                  color="info"
-                  size="small"
-                  startIcon={<GitHubIcon />}
-                  href="https://github.com/librarykrystal/art-portfolio"
-                  >See it on GitHub
-                </Button>
-            </div>
-          </div>
-        </div>
-
-        {/* THE RUNDOWN */}
-        <div className="projectContainer">
-            <div className="projectHeaderBox">
-              <Typography className="projectHeaderText" variant="h5" mt={0} mb={0} fontWeight="bold" gutterBottom>
-                The Rundown
-              </Typography>
-              <Typography className="projectHeaderText" variant="body1" mt={0} mb={0} gutterBottom>
-                Artist's Paint Inventory & Wishlist App
-              </Typography>
-            </div>
-            <div className="projectBody">
-              <div className="projectImages">
-                <img className="appImageRundown" src={rundownHome}></img>
-                <img className="appImageRundown" src={rundownDetails}></img>
-                <img className="appImageRundown" src={rundownEdit}></img>
-              </div>
-              <div className="projDescText">
-                <Typography variant="body1" mt={0} mb={0} gutterBottom>
-                This two-week solo project lets users inventory, wishlist, and filter artist's paint supplies. This app uses React, Redux, postgreSQL, CSS, and Material-UI.
-                </Typography>
-              </div>
-              <div className="projectButtons">
-                <Button
-                variant="contained"
-                color="info"
-                size="small"
-                startIcon={<GitHubIcon />}
-                href="https://github.com/librarykrystal/artist-inventory"
-                >See it on GitHub
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        {/* IDEAL LIVING WEEK */}
-        <div className="projectContainer">
-          <div className="projectHeaderBox">
-            <Typography className="projectHeaderText" variant="h5" mt={0} mb={0} fontWeight="bold" gutterBottom>
-              Ideal Living Week
-            </Typography>
-            <Typography className="projectHeaderText" variant="body1" mt={0} mb={0} gutterBottom>
-              Mindfulness and Scheduling App
-            </Typography>
-          </div>
-          <div className="projectBody">
-            <div className="projectImages">
-              <img className="appImage2" src={idealQuestions}></img>
-              <img className="appImage2" src={idealPriorities}></img>
-            </div>
-            <div className="projDescText">
-                <Typography variant="body1" mt={0} mb={0} gutterBottom>
-                This group project for client Mick White's 100 Year Manifesto includes drag-and-drop prioritization, journaling, and scheduling out an ideal week.
-                </Typography>
-              </div>
-            <div className="projectButtons">
-              <Button
-              variant="contained"
-              color="info"
-              size="small"
-              startIcon={<GitHubIcon />}
-              href="https://github.com/librarykrystal/100ym-ideal-living-week"
-              >See it on GitHub
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        {/* MY MOVIE SET */}
-        <div className="projectContainer">
-          <div className="projectHeaderBox">
-            <Typography className="projectHeaderText" variant="h5" mt={0} mb={0} fontWeight="bold" gutterBottom>
-              My Movie Set
-            </Typography>
-            <Typography className="projectHeaderText" variant="body1" mt={0} mb={0} gutterBottom>
-              Film Cataloguing App
-            </Typography>
-          </div>
-          <div className="projectBody">
-            <div className="projectImages">
-              <img className="appImage2" src={movieSetList}></img>
-              <img className="appImage2" src={movieSetDetails}></img>
-            </div>
-            <div className="projDescText">
-                <Typography variant="body1" mt={0} mb={0} gutterBottom>
-                This weekend challenge displays a collection of clickable film posters from a database.  A details page shows description and genre(s).
-                </Typography>
-              </div>
-            <div className="projectButtons">
-              <Button
-              variant="contained"
-              color="info"
-              size="small"
-              startIcon={<GitHubIcon />}
-              href="https://github.com/librarykrystal/weekend-movie-sagas"
-              >See it on GitHub
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        {/* MY SNAPS GALLERY */}
-        <div className="projectContainer">
-          <div className="projectHeaderBox">
-            <Typography className="projectHeaderText" variant="h5" mt={0} mb={0} fontWeight="bold"  gutterBottom>
-              My Snaps Gallery
-            </Typography>
-            <Typography className="projectHeaderText" variant="body1" mt={0} mb={0} gutterBottom>
-              Gallery with Likes & Toggle Captions
-            </Typography>
-          </div>
-          <div className="projectBody">
-            <div className="projectImages">
-              <img className="appImage2" src={galleryDesc2}></img>
-              <img className="appImage2" src={galleryDesc1}></img>
-            </div>
-            <div className="projDescText">
-                <Typography variant="body1" mt={0} mb={0} gutterBottom>
-                This weekend challenge showcases a set of photographs.  Each photo has a like counter and can be clicked to toggle between image and caption.
-                </Typography>
-              </div>
-            <div className="projectButtons">
-              <Button
-              variant="contained"
-              color="info"
-              size="small"
-              startIcon={<GitHubIcon />}
-              href="https://github.com/librarykrystal/weekend-react-gallery"
-              >See it on GitHub
-              </Button>
-            </div>
-          </div>
-        </div>
+            );
+          })}
         </div>
 
         <center>
